b827a300fbec8332cc4ad3823adcd3c527736569
Ilko Ivanov <ilko.iv@gmail.com>
2025-11-30 05:53:33 +0000

    display home hook

--- dskpayment.php
+++ dskpayment.php
@@ -378,46 +378,43 @@ class Dskpayment extends PaymentModule
         $dskapi_reklama = (int)Configuration::get('dskapi_reklama');
         $dskapi_status = (int)Configuration::get('dskapi_status');
 
-        if (($dskapi_reklama > 0) && ($dskapi_status > 0)) {
-            $dskapi_ch = curl_init();
-            curl_setopt($dskapi_ch, CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($dskapi_ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($dskapi_ch, CURLOPT_MAXREDIRS, 2);
-            curl_setopt($dskapi_ch, CURLOPT_TIMEOUT, 6);
-            curl_setopt($dskapi_ch, CURLOPT_URL, DSKAPI_LIVEURL . '/function/getrek.php?cid=' . $dskapi_cid);
-            $paramsdskapi = json_decode(curl_exec($dskapi_ch), true);
-            curl_close($dskapi_ch);
-
-            $useragent = array_key_exists('HTTP_USER_AGENT', $_SERVER) ? $_SERVER['HTTP_USER_AGENT'] : '';
-            if (preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i', $useragent) || preg_match('/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i', substr($useragent, 0, 4))) {
-                $dskapi_deviceis = "mobile";
-            } else {
-                $dskapi_deviceis = "pc";
-            }
-
-            $dskapi_picture = $paramsdskapi['dsk_picture'];
-            $dskapi_container_txt1 = $paramsdskapi['dsk_container_txt1'];
-            $dskapi_container_txt2 = $paramsdskapi['dsk_container_txt2'];
-            $dskapi_logo_url = $paramsdskapi['dsk_logo_url'];
-
-            if ((!empty($paramsdskapi)) && ($paramsdskapi['dsk_status'] == 1) && ($paramsdskapi['dsk_container_status'] == 1)) {
-                $this->context->smarty->assign(
-                    array(
-                        'dskapi_deviceis' => $dskapi_deviceis,
-                        'DSKAPI_LIVEURL' => DSKAPI_LIVEURL,
-                        'dskapi_picture' => $dskapi_picture,
-                        'dskapi_container_txt1' => $dskapi_container_txt1,
-                        'dskapi_container_txt2' => $dskapi_container_txt2,
-                        'dskapi_logo_url' => $dskapi_logo_url
-                    )
-                );
-                return $this->display(__FILE__, 'dskapipanel.tpl');
-            } else {
-                return null;
-            }
-        } else {
-            return null;
+        if ($dskapi_reklama == 0 || $dskapi_status == 0) {
+            return '';
         }
+
+        $paramsdskapi = $this->makeApiRequest('/function/getrek.php?cid=' . urlencode($dskapi_cid), 6);
+        if ($paramsdskapi === null) {
+            return '';
+        }
+
+        $dskapi_deviceis = $this->isMobileDevice() ? "mobile" : "pc";
+
+        $dskapi_picture = $paramsdskapi['dsk_picture'];
+        $dskapi_container_txt1 = $paramsdskapi['dsk_container_txt1'];
+        $dskapi_container_txt2 = $paramsdskapi['dsk_container_txt2'];
+        $dskapi_logo_url = $paramsdskapi['dsk_logo_url'];
+        $dskapi_logo = Media::getMediaPath(_PS_MODULE_DIR_ . $this->name . '/logo.png');
+
+        if (
+            (int)$paramsdskapi['dsk_status'] !== 1 ||
+            (int)$paramsdskapi['dsk_container_status'] !== 1
+        ) {
+            return '';
+        }
+
+        $this->context->smarty->assign(
+            array(
+                'dskapi_deviceis' => $dskapi_deviceis,
+                'DSKAPI_LIVEURL' => DSKAPI_LIVEURL,
+                'dskapi_picture' => $dskapi_picture,
+                'dskapi_container_txt1' => $dskapi_container_txt1,
+                'dskapi_container_txt2' => $dskapi_container_txt2,
+                'dskapi_logo_url' => $dskapi_logo_url,
+                'dskapi_logo' => $dskapi_logo
+            )
+        );
+
+        return $this->display(__FILE__, 'dskapipanel.tpl');
     }
 
     public function hookPaymentOptions($params)
@@ -1112,5 +1109,69 @@ class Dskpayment extends PaymentModule
         } else {
             return null;
         }
+    }
+
+    /**
+     * Извършва API заявка и връща декодирания JSON отговор
+     *
+     * @param string $endpoint API endpoint (без базовия URL)
+     * @param int $timeout Timeout в секунди
+     * @return array|null Декодираният JSON отговор или null при грешка
+     */
+    /**
+     * Executes an HTTP request to the DSK API and returns the decoded response.
+     *
+     * @param string $endpoint Relative API endpoint path
+     * @param int $timeout Request timeout in seconds
+     *
+     * @return array|null
+     */
+    private function makeApiRequest(string $endpoint, int $timeout = 5): ?array
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_MAXREDIRS, 2);
+        curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+        curl_setopt($ch, CURLOPT_URL, DSKAPI_LIVEURL . $endpoint);
+
+        $response = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $curlError = curl_error($ch);
+        curl_close($ch);
+
+        if ($response === false || $httpCode !== 200 || !empty($curlError)) {
+            return null;
+        }
+
+        $decoded = json_decode($response, true);
+        if (json_last_error() !== JSON_ERROR_NONE || !is_array($decoded)) {
+            return null;
+        }
+
+        return $decoded;
+    }
+
+    /**
+     * Проверява дали устройството е мобилно
+     *
+     * @return bool
+     */
+    /**
+     * Detects whether the current visitor uses a mobile device.
+     *
+     * @return bool
+     */
+    private function isMobileDevice(): bool
+    {
+        $useragent = $_SERVER['HTTP_USER_AGENT'] ?? '';
+        if (empty($useragent)) {
+            return false;
+        }
+
+        $mobilePattern = '/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i';
+
+        return (bool) preg_match($mobilePattern, $useragent)
+            || (bool) preg_match('/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i', substr($useragent, 0, 4));
     }
 }
--- views/templates/hook/dskapipanel.tpl
+++ views/templates/hook/dskapipanel.tpl
@@ -4,23 +4,24 @@
     * @Author e-mail: ilko.iv@gmail.com
     * @Publisher: Avalon Ltd
     * @Publisher e-mail: home@avalonbg.com
-    * @Owner: Avalon Ltd
-    * @Version: 1.1.0
+    * @Owner: Банка ДСК
+    * @Version: 1.2.0
 *}
 {if $dskapi_deviceis eq 'mobile'}
-<div class="dskapi_float" onclick="window.open('{$DSKAPI_LIVEURL}/procedure.php', '_blank');">
-{else}
-<div class="dskapi_float" onclick="DskapiChangeContainer();">
-{/if}
-    <img src="{$DSKAPI_LIVEURL}/dist/img/dsk_logo.png" class="dskapi-my-float">
-</div>
-<div class="dskapi-label-container">
-    <div class="dskapi-label-text">
-        <div class="dskapi-label-text-mask">
-            <img src="{$dskapi_picture}" class="dskapi_header">
-            <p class="dskapi_txt1">{$dskapi_container_txt1}</p>
-            <p class="dskapi_txt2">{$dskapi_container_txt2}</p>
-            <p class="dskapi-label-text-a"><a href="{$dskapi_logo_url}" target="_blank" alt="За повече информация">За повече информация</a></p>
-        </div>
+    <div class="dskapi_float" onclick="window.open('{$DSKAPI_LIVEURL}/procedure.php', '_blank');">
+    {else}
+        <div class="dskapi_float" onclick="DskapiChangeContainer();">
+        {/if}
+        <img src="{$dskapi_logo}" class="dskapi-my-float">
     </div>
+    <div class="dskapi-label-container">
+        <div class="dskapi-label-text">
+            <div class="dskapi-label-text-mask">
+                <img src="{$dskapi_picture}" class="dskapi_header">
+                <p class="dskapi_txt1">{$dskapi_container_txt1}</p>
+                <p class="dskapi_txt2">{$dskapi_container_txt2}</p>
+                <p class="dskapi-label-text-a"><a href="{$dskapi_logo_url}" target="_blank"
+                        alt="За повече информация">За повече информация</a></p>
+            </div>
+        </div>
 </div>
